c1033dc87109f5834798d37f5186e0fb786cef0e
Kevin Tolan <82405167+kevtolan@users.noreply.github.com>
2026-03-31 13:27:02 -0400

    Update suitable_habitat.R

--- suitable_habitat.R
+++ suitable_habitat.R
@@ -22,6 +22,10 @@ library(patchwork)
 # bobo_conservation_plannding <- st_read('~/QGIS/EAME/BOBO_Opp_Map.shp')
 
 # download political boundaries
+
+landtrans <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Cadastral_PTTR_point_WM_v1_view/FeatureServer/0",
+                           out_fields = c("span")) %>% st_transform(crs = 32145) %>% st_make_valid()
+
 towns <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Boundary_BNDHASH_poly_towns_SP_v1/FeatureServer/0",
                                    out_fields = c("TOWNNAMEMC")) %>% st_transform(crs = 32145) %>% st_make_valid()
 
@@ -41,7 +45,7 @@ VT_soils <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/Arc
                               filter(!PRIME %in% c("NPSL","Not rated", "", " ")) %>%
                               st_simplify(dTolerance = 1, preserveTopology = T) %>%
                               rename(SoilID = OBJECTID,
-                                     SoilType = PRIME)
+                                     SoilClass = PRIME)
 
 temp_zip22 <- tempfile(fileext = ".zip")
 temp_zip16 <- tempfile(fileext = ".zip")
@@ -77,7 +81,7 @@ VT_hay_pasture_22_parcels <- VT_hay_pasture_22_soils %>%
   st_intersection(parcels) %>%
   filter(st_geometry_type(Shape) %in% c("POLYGON", "MULTIPOLYGON"))
 
-# st_write(VT_hay_pasture_22_parcels,"~/R/Grasslab hab/VT_hay_pasture_22.shp")
+# st_write(VT_hay_pasture_22_parcels,"~/R/Grasslab hab/VT_grasslands_2022.shp")
 
 VT_hay_pasture_16 <- st_read(dsn = paste0(temp_dir, "/LandLandcov_Agriculture2016/LandLandcov_Agriculture2016.gdb"),
                              layer = "LandLandcov_Agriculture2016_poly") %>%
@@ -96,21 +100,34 @@ VT_hay_pasture_16 <- st_read(dsn = paste0(temp_dir, "/LandLandcov_Agriculture201
   dplyr::select(!c('gridcode'))
 
 VT_hay_pasture_16_soils <- VT_hay_pasture_16 %>%
-  st_intersection(VT_soils)
+  st_intersection(VT_soils) %>% st_make_valid()
 
 VT_hay_pasture_16_parcels <- VT_hay_pasture_16_soils %>%
   st_intersection(parcels) %>%
   filter(st_geometry_type(Shape) %in% c("POLYGON", "MULTIPOLYGON"))
 
-mapview(VT_hay_pasture_16_parcels)
 
-# st_write(VT_hay_pasture_16_soils,"~/R/Grasslab hab/VT_hay_pasture_16_soils.geojson")
+# st_write(VT_hay_pasture_16_parcels,"~/R/Grasslab hab/VT_grasslands_2016.shp")
 
-hay_16_union <- st_union(st_geometry(VT_hay_pasture_16_parcels))
+
+hay_16_union <- st_union(st_geometry(VT_hay_pasture_16))
 hay_22_union <- st_union(st_geometry(VT_hay_pasture_22))
 
-VT_lost_gland <- st_difference(VT_hay_pasture_16_parcels, hay_22_union) %>%
-  mutate(Status = "lost")
+VT_lost_gland <- st_difference(VT_hay_pasture_16_soils, hay_22_union) %>%
+  st_make_valid() %>%
+  st_collection_extract("POLYGON") %>%
+  st_cast("POLYGON") %>%
+  mutate(Status = "lost",
+         Shape_Area = as.numeric(st_area(Shape)),
+         area_ha = Shape_Area/10000,
+         area_ac = Shape_Area*0.000247105) %>%
+  filter(Shape_Area > 0)
 
-VT_gained_gland <- st_difference(VT_hay_pasture_22_parcels, hay_16_union) %>%
-  mutate(Status = "gained")
+
+
+VT_gained_gland <- st_difference(VT_hay_pasture_22_soils, hay_16_union) %>%
+  mutate(Status = "gained") %>%
+  mutate(Shape_Area = st_area(Shape),
+         area_ha = Shape_Area/10000,
+         area_ac = Shape_Area*0.000247105) %>%
+  filter(Shape_Area > 0)
