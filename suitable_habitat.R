9ae9117017b22a67467eebc2359ebdf45bf9ea0f
Kevin Tolan <82405167+kevtolan@users.noreply.github.com>
2026-03-30 20:22:51 -0400

    Update suitable_habitat.R

--- suitable_habitat.R
+++ suitable_habitat.R
@@ -15,37 +15,44 @@ library(timetk)
 library(magick)
 library(sf)
 library(cowplot)
-library(arcpullr) # download ESRI-hosted data
+library(arcpullr)
 library(ggspatial)
 library(cowplot)
 library(patchwork)
+# bobo_conservation_plannding <- st_read('~/QGIS/EAME/BOBO_Opp_Map.shp')
 
 # download political boundaries
 towns <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Boundary_BNDHASH_poly_towns_SP_v1/FeatureServer/0",
                                    out_fields = c("TOWNNAMEMC")) %>% st_transform(crs = 32145) %>% st_make_valid()
 
 counties <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Boundary_BNDHASH_poly_counties_SP_v1/FeatureServer/0",
-                           out_fields = c("CNTYNAME")) %>% st_transform(crs = 32145) %>% st_make_valid()
+                              out_fields = c("CNTYNAME")) %>% st_transform(crs = 32145) %>% st_make_valid()
+
 
 parcels <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Cadastral_VTPARCELS_poly_standardized_parcels_SP_v1/FeatureServer/0",
-                             out_fields = c("OBJECTID","SPAN")) %>% st_transform(crs = 32145) %>% st_make_valid()
+                             out_fields = c("SPAN","YEAR","OWNER1","OWNER2","ADDRGL1","ADDRGL2",
+                                            "CITYGL","STGL","ZIPGL","E911ADDR","DESCPROP","LOCAPROP",
+                                            "REAL_FLV","HSTED_FLV","NRES_FLV","LAND_LV")) %>%
+                             st_transform(crs = 32145) %>% st_make_valid()
 
 
-VT_soils <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Geologic_SO_poly_SP_v1/FeatureServer/0",
+VT_soils <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Geologic_SOAG_poly_SP_v1/FeatureServer/0",
                               out_fields = c("OBJECTID","PRIME")) %>% st_transform(crs = 32145) %>% st_make_valid() %>%
                               filter(!PRIME %in% c("NPSL","Not rated", "", " ")) %>%
-                              st_simplify(dTolerance = 1, preserveTopology = T)
-
-# bobo_conservation_plannding <- st_read('~/QGIS/EAME/BOBO_Opp_Map.shp')
+                              st_simplify(dTolerance = 1, preserveTopology = T) %>%
+                              rename(SoilID = OBJECTID,
+                                     SoilType = PRIME)
 
 temp_zip22 <- tempfile(fileext = ".zip")
 temp_zip16 <- tempfile(fileext = ".zip")
 temp_dir <- tempdir()
 
-download.file("https://s3.us-east-2.amazonaws.com/vtopendata-prd/Landcover/_Packaged_Zips/LandLandcov_Agriculture2022.zip", destfile = temp_zip22, mode = "wb")
+download.file("https://s3.us-east-2.amazonaws.com/vtopendata-prd/Landcover/_Packaged_Zips/LandLandcov_Agriculture2022.zip",
+                  destfile = temp_zip22, mode = "wb")
 unzip(temp_zip22, exdir = temp_dir)
 
-download.file("https://s3.us-east-2.amazonaws.com/vtopendata-prd/Landcover/_Packaged_Zips/LandLandcov_Agriculture2016.zip", destfile = temp_zip16, mode = "wb")
+download.file("https://s3.us-east-2.amazonaws.com/vtopendata-prd/Landcover/_Packaged_Zips/LandLandcov_Agriculture2016.zip",
+                  destfile = temp_zip16, mode = "wb")
 unzip(temp_zip16, exdir = temp_dir)
 
 VT_hay_pasture_22 <- st_read(dsn = paste0(temp_dir, "/LandLandcov_Agriculture2022/LandLandcov_Agriculture2022.gdb"),
@@ -59,16 +66,18 @@ VT_hay_pasture_22 <- st_read(dsn = paste0(temp_dir, "/LandLandcov_Agriculture202
   filter(perim_area <= 0.03,
          area_ac >= 10) %>%
   st_simplify(dTolerance = 1, preserveTopology = T) %>%
-  st_intersection(parcels) %>%
   st_intersection(towns) %>%
   st_intersection(counties) %>%
   dplyr::select(!c('gridcode'))
 
-
 VT_hay_pasture_22_soils <- VT_hay_pasture_22 %>%
   st_intersection(VT_soils)
 
-st_write(VT_hay_pasture_22_soils,"~/R/Grasslab hab/VT_hay_pasture_22_soils.geojson")
+VT_hay_pasture_22_parcels <- VT_hay_pasture_22_soils %>%
+  st_intersection(parcels) %>%
+  filter(st_geometry_type(Shape) %in% c("POLYGON", "MULTIPOLYGON"))
+
+# st_write(VT_hay_pasture_22_parcels,"~/R/Grasslab hab/VT_hay_pasture_22.shp")
 
 VT_hay_pasture_16 <- st_read(dsn = paste0(temp_dir, "/LandLandcov_Agriculture2016/LandLandcov_Agriculture2016.gdb"),
                              layer = "LandLandcov_Agriculture2016_poly") %>%
@@ -86,17 +95,22 @@ VT_hay_pasture_16 <- st_read(dsn = paste0(temp_dir, "/LandLandcov_Agriculture201
   st_intersection(counties) %>%
   dplyr::select(!c('gridcode'))
 
-
 VT_hay_pasture_16_soils <- VT_hay_pasture_16 %>%
   st_intersection(VT_soils)
 
-st_write(VT_hay_pasture_16_soils,"~/R/Grasslab hab/VT_hay_pasture_16_soils.geojson")
+VT_hay_pasture_16_parcels <- VT_hay_pasture_16_soils %>%
+  st_intersection(parcels) %>%
+  filter(st_geometry_type(Shape) %in% c("POLYGON", "MULTIPOLYGON"))
 
-hay_16_union <- st_union(st_geometry(VT_hay_pasture_16))
+mapview(VT_hay_pasture_16_parcels)
+
+# st_write(VT_hay_pasture_16_soils,"~/R/Grasslab hab/VT_hay_pasture_16_soils.geojson")
+
+hay_16_union <- st_union(st_geometry(VT_hay_pasture_16_parcels))
 hay_22_union <- st_union(st_geometry(VT_hay_pasture_22))
 
-VT_lost_gland <- st_difference(VT_hay_pasture_16, hay_22_union) %>%
+VT_lost_gland <- st_difference(VT_hay_pasture_16_parcels, hay_22_union) %>%
   mutate(Status = "lost")
 
-VT_gained_gland <- st_difference(VT_hay_pasture_22, hay_16_union) %>%
+VT_gained_gland <- st_difference(VT_hay_pasture_22_parcels, hay_16_union) %>%
   mutate(Status = "gained")
