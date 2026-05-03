e077da9c2c1134377edfaf6166189a0c79b27d24
Kevin Tolan <82405167+kevtolan@users.noreply.github.com>
2026-05-02 21:40:31 -0400

    Update suitable_habitat.R

--- suitable_habitat.R
+++ suitable_habitat.R
@@ -21,7 +21,6 @@ library(cowplot)
 library(patchwork)
 # bobo_conservation_plannding <- st_read('~/QGIS/EAME/BOBO_Opp_Map.shp')
 
-
 temp_zip22 <- tempfile(fileext = ".zip")
 temp_dir <- tempdir()
 
@@ -41,18 +40,18 @@ VT_hay_pasture_22 <- st_read(dsn = paste0(temp_dir, "/LandLandcov_Agriculture202
          perim_area = Shape_Length/Shape_Area,
          sourceyear = '2022') %>%
   filter(#perim_area <= 0.03,
-    area_ac >= 1) %>%
+    area_ac >= 5) %>%
   st_simplify(dTolerance = 1, preserveTopology = T) %>%
   dplyr::select(!c('gridcode'))
 
 
 landtrans <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Cadastral_PTTR_point_WM_v1_view/FeatureServer/0",
-                               out_fields = c("addBuyrLoc", "addBuyrNam", "addSellLoc", "addSellNam",
-                                              "buyrAdjPrp", "buyEntNam", "buyFstNam", "buyLstNam",
-                                              "buyerState", "buyerStrt", "bUsePr", "bUsePrDesc",
-                                              "closeDate", "enrCrntUse", "LGTExDesc", "prTxExDesc",
-                                              "sellAqDesc", "sellFstNam", "sellLstNam", "sellerSt",
-                                              "sUsePr", "sUsePrDesc", "sUsePrExpl", "TownGlValu")) %>% st_transform(crs = 32145) %>% st_make_valid()
+                               out_fields = c("addBuyrLoc", "addBuyrNam", "addSellLoc", "addSellNam","TownGlValu",
+                                              # "buyrAdjPrp", "buyEntNam", "buyFstNam", "buyLstNam","sUsePrExpl",
+                                              # "buyerState", "buyerStrt", "bUsePr", "bUsePrDesc","sUsePrDesc",
+                                              # "closeDate", "enrCrntUse", "LGTExDesc", "prTxExDesc","sUsePr",
+                                              # "sellAqDesc", "sellFstNam", "sellLstNam", "sellerSt",
+                                              )) %>% st_transform(crs = 32145) %>% st_make_valid()
 
 parcels <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Cadastral_VTPARCELS_poly_standardized_parcels_SP_v1/FeatureServer/0",
                              out_fields = c("SPAN","YEAR","OWNER1","OWNER2","ADDRGL1","ADDRGL2",
@@ -62,6 +61,8 @@ parcels <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcG
 
 parcels_joined <- parcels %>%
   mutate(has_landtrans = as.factor(as.integer(lengths(st_intersects(., landtrans)) > 0)))
+rm(parcels)
+rm(landtrans)
 
 VT_sig_soils <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Geologic_SOAG_poly_SP_v1/FeatureServer/0",
                                   out_fields = c("OBJECTID","PRIME")) %>% st_transform(crs = 32145) %>% st_make_valid() %>%
@@ -86,6 +87,9 @@ VT_hay_pasture_22_parcels <- soils_clean %>% st_intersection(parcels_joined) %>%
   filter(st_geometry_type(Shape) %in% c("POLYGON", "MULTIPOLYGON")) %>%
   mutate(rowID = row_number())
 
+saveRDS(VT_hay_pasture_22_parcels, "VT_hay_pasture_22_parcels.rds")
+VT_hay_pasture_22_parcels <- readRDS('VT_hay_pasture_22_parcels.rds')
+
 output_dir <- file.path(temp_dir, "masked_openness")
 if(!dir.exists(output_dir)) dir.create(output_dir)
 
@@ -93,46 +97,33 @@ results_list <- list()
 rm(r, r_masked, hay_sf, hay_vect, hay_vect_cropped, means)
 gc()
 
+
 for (i in seq_along(openness_files)) {
+  # i <- 263
+# for (i in 264:length(openness_files)) {
+# for (i in 1:262) {
+  r <- rast(openness_files[i])
 
-  r_path <- openness_files[i]
-  r <- rast(r_path)
-
-  hay_vect_cropped <- suppressWarnings(st_crop(VT_hay_pasture_22_parcels, r))
+  hay_vect_cropped <- suppressWarnings(st_crop(VT_hay_pasture_22_parcels, ext(r)))
 
   if (nrow(hay_vect_cropped) == 0) {
     message(paste0("Skipping tile ", i, ": No overlapping parcels found."))
-    next
-  }
+    next }
 
-  hay_vect_cropped <- st_make_valid(hay_vect_cropped)
-  hay_vect_cropped <- hay_vect_cropped[!st_is_empty(hay_vect_cropped), ]
-  hay_vect_cropped <- st_collection_extract(hay_vect_cropped, "POLYGON")
+  hay_vect_cropped <- st_make_valid(hay_vect_cropped) %>% .[!st_is_empty(.), ]
 
-  if (nrow(hay_vect_cropped) == 0) {
-    message(paste0("Skipping tile ", i, ": No valid geometries after repair."))
-    next
-  }
+    if (inherits(st_geometry(hay_vect_cropped), "sfc_GEOMETRY")) {
+    hay_vect_cropped <- st_collection_extract(hay_vect_cropped, "POLYGON") }
 
-  hay_vect <- vect(hay_vect_cropped)
+  means <- exact_extract(r, hay_vect_cropped, 'mean', progress = FALSE)
 
-  if (crs(r) != crs(hay_vect)) {
-    r <- project(r, crs(hay_vect))
-  }
+  results_list[[i]] <- hay_vect_cropped %>% st_drop_geometry() %>%
+    mutate(Open_decimal = means, tile_id = i)
 
-  r_masked <- crop(r, hay_vect) %>% mask(hay_vect)
+  rm(r, hay_vect_cropped, means)
+  if (i %% 10 == 0) gc()
 
-  hay_sf <- st_as_sf(hay_vect)
-  means <- exact_extract(r_masked, hay_sf, 'mean', progress = FALSE)
-  hay_sf$Open_decimal <- means
-
-  results_list[[i]] <- st_drop_geometry(hay_sf)
-
-  rm(r, r_masked, hay_sf, hay_vect, hay_vect_cropped, means)
-  gc()
-
-  message(paste0("Finished tile ", i, " of ", length(openness_files)))
-}
+  message(paste0("Finished tile ", i, " of ", length(openness_files))) }
 
 
 all_stats <- bind_rows(results_list) %>%
@@ -142,6 +133,40 @@ all_stats <- bind_rows(results_list) %>%
 hay_pasture_stats <- VT_hay_pasture_22_parcels %>%
   left_join(all_stats, by = "rowID")
 
+towns <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Boundary_BNDHASH_poly_towns_SP_v1/FeatureServer/0",
+                           out_fields = c("TOWNNAMEMC")) %>% st_transform(crs = 32145) %>% st_make_valid()
 
-mapview(hay_pasture_stats[hay_pasture_stats$mean_openness > 0.7,] %>% filter(!is.na(mean_openness)), zcol = "mean_openness")
+counties <- get_spatial_layer("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/ArcGIS/rest/services/FS_VCGI_OPENDATA_Boundary_BNDHASH_poly_counties_SP_v1/FeatureServer/0",
+                              out_fields = c("CNTYNAME")) %>% st_transform(crs = 32145) %>% st_make_valid()
+
+hay_pasture_stats_towns <- hay_pasture_stats %>%
+  st_intersection(towns) %>%
+  st_intersection(counties)
+
+saveRDS(hay_pasture_stats_towns, "hay_pasture_stats_towns.RDS")
+
+
+hay_polys <- hay_pasture_stats_towns[st_is(hay_pasture_stats_towns, c("POLYGON", "MULTIPOLYGON")), ] %>%
+  st_cast("MULTIPOLYGON") %>%
+  as.data.frame() %>%
+  st_as_sf()
+
+
+hay_list <- split(hay_polys, hay_polys$CNTYNAME)
+
+# prefix <- "~/R/Grasslab hab/Combined_open/hay_polys"
+
+timestamp <- format(Sys.time(), "%Y%m%d_%H%M")
+
+lapply(names(hay_list), function(county) {
+
+  file_name <- sprintf("%s_%s_%s.shp", "~/R/Grasslab hab/Combined_open/hay_polys", county, timestamp)
+
+  st_write(hay_list[[county]], file_name, delete_dsn = TRUE)
+})
+
+
+
+
+
 
