b771c61c0e5588988e3d51eb5704ca8fac99a1df
Kevin Tolan <82405167+kevtolan@users.noreply.github.com>
2026-05-09 13:45:30 -0400

    Update field_openness_loop.R

--- field_openness_loop.R
+++ field_openness_loop.R
@@ -37,10 +37,12 @@ VT_agr_22 <- st_read(dsn = paste0(td, "/LandLandcov_Agriculture2022/LandLandcov_
   st_simplify(dTolerance = 1, preserveTopology = T) %>%
   dplyr::select(!c('gridcode'))
 
-vt_blocks <- get_spatial_layer("https://services1.arcgis.com/d3OaJoSAh2eh6OA9/ArcGIS/rest/services/Vermont_Wildlife_Atlasing_Blocks/FeatureServer/0",
-                               out_fields = c("BLOCKNAME","QUADNAME","GEOUNITDES")) %>% st_transform(crs = 32145) # download grid
+# vt_blocks <- get_spatial_layer("https://services1.arcgis.com/d3OaJoSAh2eh6OA9/ArcGIS/rest/services/Vermont_Wildlife_Atlasing_Blocks/FeatureServer/0",
+                               # out_fields = c("BLOCKNAME","QUADNAME","GEOUNITDES")) %>% st_transform(crs = 32145) # download grid
+vt_blocks <- st_read('/Users/kevintolan/R/R_Spatial/BBA_Blocks/VT03_grid.shp') %>% st_transform(crs = 32145)
 
 aoi <- vt_blocks
+
 files <- list.files(out_dir, pattern = "\\.tif$")
 existing_blocks <- gsub("combined_sf_|\\.geojson", "", files)
 aoi <- aoi[order(!(aoi$BLOCKNAME %in% existing_blocks)), ] # sort existing files to top
@@ -53,36 +55,23 @@ dsm_cog_url <- dsm_cog_url_central
 raster_extent <- ext(dsm_cog_url)
 bbox_poly <- as.polygons(raster_extent, crs = crs(dsm_cog_url)) %>% project("EPSG:32145")
 cropped_blocks <- st_crop(vt_blocks, bbox_poly)
-bb <- st_bbox(cropped_blocks)
-
-# width <- bb[["xmax"]] - bb[["xmin"]]
-# western_cutoff <- bb[["xmin"]] + (width * 0.25)
-#
-# west_coords <- matrix(c(
-#   bb[["xmin"]], bb[["ymin"]],
-#   western_cutoff, bb[["ymin"]],
-#   western_cutoff, bb[["ymax"]],
-#   bb[["xmin"]], bb[["ymax"]],
-#   bb[["xmin"]], bb[["ymin"]]),
-#   ncol = 2, byrow = TRUE)
-
-# width <- bb[["xmax"]] - bb[["xmin"]]
-#
-# center_start <- bb[["xmin"]] + (width * 0.25)
-# center_end   <- bb[["xmin"]] + (width * 0.75)
-# west_coords <- matrix(c(
-#   center_start, bb[["ymin"]],
-#   center_end,   bb[["ymin"]],
-#   center_end,   bb[["ymax"]],
-#   center_start, bb[["ymax"]],
-#   center_start, bb[["ymin"]]),
-#   ncol = 2, byrow = TRUE)
+bb <- st_bbox(cropped_blocks) %>% st_as_sfc() %>% st_as_sf()
 
 # subset_poly <- st_sfc(st_polygon(list(west_coords)), crs = st_crs(cropped_blocks))
 # blocks_subset <- cropped_blocks[st_intersects(cropped_blocks, subset_poly, sparse = FALSE), ]
-# blocks <- unique(blocks_subset$BLOCKNAME)
-blocks <- unique(cropped_blocks$BLOCKNAME)
+blocks_subset <- cropped_blocks[st_intersects(cropped_blocks, bb, sparse = FALSE), ]
 
+
+all_out_files <- file.path(out_dir, paste0(blocks, "_open.tif"))
+
+# 2. Check which ones DON'T exist (!)
+missing_indices <- !file.exists(all_out_files)
+
+blocks_to_process <- blocks[missing_indices]
+
+blocks <- unique(blocks_subset$BLOCKNAME)
+
+# blocks <- unique(cropped_blocks$BLOCKNAME)
 
 plan(multisession, workers = 8)
 future_walk(seq_along(blocks), function(counter) {
